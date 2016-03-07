a9e5c6a3eb58803d6145e875b7372ef9ec582fa7
dflores <dflores@MF-OPERADOR.mifarma.com.pe>
2016-03-07 18:08:25 -0500

    Love commit_07.03.16!

--- src/java/pruebas/Prueba01.java
+++ src/java/pruebas/Prueba01.java
@@ -1,12 +1,32 @@
 
 package pruebas;
 
+import java.util.Iterator;
+import model.Departamentos;
+import model.HibernateUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 public class Prueba01 {
     
     public static void main(String[] args) {
         
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         
+        session.beginTransaction();
+        String s = "from Departamentos";
+        Query query = session.createQuery(s);
+        Iterator it = query.iterate();
         
+        while (it.hasNext()) {
+            Departamentos departamentos = (Departamentos) it.next();
+            System.out.println(
+                    departamentos.getIddepartamento() + " - "
+                    + departamentos.getDepartamento());
+        }
+        
+        session.close();
     }
     
 }
